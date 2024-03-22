fda7e772b3561348f9741956c3be4a6c6d2a51fd
Matheus Link <linksauermths2203@gmail.com>
2024-03-21 21:02:54 -0300

    Form: Refazendo o formulário, utilizando o react-bootstrap para deixar mais utilizável

--- client/src/Pages/Form/index.jsx
+++ client/src/Pages/Form/index.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
-import Input from "../../Components/Input";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
 
 const AddEmployeeForm = ({ addEmployee }) => {
+  const [validated, setValidated] = useState(false);
   const [name, setName] = useState("");
-  const [image, setImage] = useState(null)
-  const [bornDate, setBornDate] = useState("")
+  const [image, setImage] = useState("");
+  const [bornDate, setBornDate] = useState("");
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState("");
   const [contractType, setContractType] = useState("");
@@ -14,96 +19,167 @@ const AddEmployeeForm = ({ addEmployee }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!name || !position || !wage || !contractType || !start || !finish) {
-      alert("Por favor, preencha todos os campos.");
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+
+    if (
+      !name ||
+      !image ||
+      !bornDate ||
+      !position ||
+      !wage ||
+      !contractType ||
+      !start ||
+      !finish
+    ) {
       return false;
     }
 
-    addEmployee({ name, position, wage, contractType, start, finish });
+    addEmployee({
+      name,
+      image,
+      bornDate,
+      position,
+      wage,
+      contractType,
+      start,
+      finish,
+    });
     setName("");
+    setImage("");
+    setBornDate("");
     setPosition("");
     setContractType("");
     setWage("");
     setStart("");
     setFinish("");
+    setValidated(false);
   };
 
-  const inputs = [
-    {
-      label: "Name",
-      type: "text",
-      placeholder: "Nome do funcionário",
-      value: name,
-      setter: setName,
-    },
-    {
-      label: "Image",
-      type: "text",
-      placeholder: "Url de foto do funcionário",
-      value: image,
-      setter: setImage,
-    },
-    {
-      label: "Nascimento",
-      type: "date",
-      placeholder: "Data de nascimento do funcionário",
-      value: bornDate,
-      setter: setBornDate,
-    },
-    {
-      label: "Posição",
-      type: "text",
-      placeholder: "Posição do funcionário",
-      value: position,
-      setter: setPosition,
-    },
-    {
-      label: "Salário",
-      type: "number",
-      placeholder: "Salário do funcionário",
-      value: wage,
-      setter: setWage,
-    },
-    {
-      label: "Contrato",
-      type: "text",
-      placeholder: "Contrato do funcionário",
-      value: contractType,
-      setter: setContractType,
-    },
-    {
-      label: "Início",
-      type: "date",
-      placeholder: "Início do contrato",
-      value: start,
-      setter: setStart,
-    },
-    {
-      label: "Final",
-      type: "date",
-      placeholder: "Final do contrato",
-      value: finish,
-      setter: setFinish,
-    },
-  ];
-
   return (
-    <main>
-      <h3>Cadastre um Funcionário</h3>
-      <form onSubmit={handleSubmit}>
-        {inputs.map((input, index) => (
-          <Input
-            key={index}
-            inputType={input.type}
-            inputLabel={input.label}
-            placeholderText={input.placeholder}
-            value={input.value}
-            inputSetter={input.setter}
-          />
-        ))}
-        <button type="submit">Adicionar</button>
-      </form>
-    </main>
+    <>
+      <h2>Cadastre um funcionário</h2>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Nome do funcionário</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Nome do funcionário"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor, preencha este campo.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+            <Form.Label>Foto do funcionário</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="URL de foto do funcionário"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor, preencha este campo.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+            <Form.Label>Data de nascimento do funcionário</Form.Label>
+            <InputGroup hasValidation>
+              <Form.Control
+                type="date"
+                placeholder="Data de nascimento do funcionário"
+                value={bornDate}
+                onChange={(e) => setBornDate(e.target.value)}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Por favor, preencha este campo.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="6" controlId="validationCustom03">
+            <Form.Label>Posição do funcionário</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Posição do funcionário"
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor, preencha este campo.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom04">
+            <Form.Label>Salário do funcionário</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Salário do funcionário"
+              value={wage}
+              onChange={(e) => setWage(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor, preencha este campo.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom05">
+            <Form.Label>Contrato do funcionário</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Contrato do funcionário"
+              value={contractType}
+              onChange={(e) => setContractType(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor, preencha este campo.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="6" controlId="validationCustom06">
+            <Form.Label>Início do contrato</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Início do contrato"
+              value={start}
+              onChange={(e) => setStart(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor, preencha este campo.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="validationCustom07">
+            <Form.Label>Final do contrato</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Final do contrato"
+              value={finish}
+              onChange={(e) => setFinish(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor, preencha este campo.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Button type="submit">Adicionar</Button>
+      </Form>
+    </>
   );
 };
 
--- client/src/Pages/List/index.jsx
+++ client/src/Pages/List/index.jsx
@@ -1,41 +1,61 @@
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
-
+import Accordion from "react-bootstrap/Accordion";
 
 const EmployeeList = ({ employees, deleteEmployee }) => {
   return (
-    <main id="tela-lista">
-      <h3>Lista Funcionários: </h3>
-      <ul className="lista-funcionarios">
-        {employees.map((employee) => (
-          <Card key={employee.id} style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={employee.img} />
-            <Card.Body>
-              <Card.Title>{employee.name}</Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>Nascimento: {employee.bornDate}</ListGroup.Item>
-              <ListGroup.Item>Cargo: {employee.position}</ListGroup.Item>
-              <ListGroup.Item>Contrato: {employee.contractType}</ListGroup.Item>
-              <ListGroup.Item>
-                Salário: R${parseFloat(employee.wage).toLocaleString("pt-BR")}
-              </ListGroup.Item>
-              <ListGroup.Item>Começo: {employee.start}</ListGroup.Item>
-              <ListGroup.Item>Término: {employee.finish}</ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <Button
-                variant="danger"
-                onClick={() => deleteEmployee(employee.id)}
-              >
-                Remover
-              </Button>
-            </Card.Body>
-          </Card>
-        ))}
-      </ul>
-    </main>
+    <>
+      {employees && employees.length > 0 ? (
+        <>
+          <h3>Lista Funcionários: </h3>
+          <Accordion defaultActiveKey="0">
+            {employees.map((employee) => (
+              <Accordion.Item key={employee.id} eventKey={employee.id}>
+                <Accordion.Header>{employee.name}</Accordion.Header>
+                <Accordion.Body>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={employee.image} />
+                    <Card.Body>
+                      <Card.Title>{employee.name}</Card.Title>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                      <ListGroup.Item>
+                        Nascimento: {employee.bornDate}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Cargo: {employee.position}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Contrato: {employee.contractType}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        Salário: R$
+                        {parseFloat(employee.wage).toLocaleString("pt-BR")}
+                      </ListGroup.Item>
+                      <ListGroup.Item>Começo: {employee.start}</ListGroup.Item>
+                      <ListGroup.Item>
+                        Término: {employee.finish}
+                      </ListGroup.Item>
+                    </ListGroup>
+                    <Card.Body>
+                      <Button
+                        variant="danger"
+                        onClick={() => deleteEmployee(employee.id)}
+                      >
+                        Remover
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Accordion.Body>
+              </Accordion.Item>
+            ))}
+          </Accordion>
+        </>
+      ) : (
+        <h3>A lista está vazia.</h3>
+      )}
+    </>
   );
 };
 

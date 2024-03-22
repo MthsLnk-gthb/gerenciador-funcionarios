23cd3c3b01ef4089377a62409bdd276a2d89ad53
Matheus Link <linksauermths2203@gmail.com>
2024-03-22 14:51:02 -0300

    Styles: Aplicando estilos básicos

--- client/src/Pages/List/index.jsx
+++ client/src/Pages/List/index.jsx
@@ -8,48 +8,90 @@ const EmployeeList = ({ employees, deleteEmployee }) => {
     <>
       {employees && employees.length > 0 ? (
         <>
-          <h2 style={{textAlign: "center", fontSize: "1.5rem", marginTop: "5dvh"}}>Lista Funcionários: </h2>
-          <article style={{display: "flex", flexWrap: "wrap", gap: "15px", justifyContent: "center", alignItems: "center", margin: "5dvh auto"}}>
+          <h2
+            style={{
+              textAlign: "center",
+              fontSize: "1.5rem",
+              marginTop: "5dvh",
+            }}
+          >
+            Lista de funcionários:{" "}
+          </h2>
+          <article
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "15px",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "5dvh auto",
+            }}
+          >
             {employees.map((employee) => (
-                  <Card style={{ width: "18rem" }} key={employee.name}>
-                    <Card.Img variant="top" src={employee.image} />
-                    <Card.Body>
-                      <Card.Title>{employee.name}</Card.Title>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                      <ListGroup.Item>
-                        Nascimento: {employee.bornDate}
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        Cargo: {employee.position}
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        Contrato: {employee.contractType}
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        Salário: R$
-                        {parseFloat(employee.wage).toLocaleString("pt-BR")}
-                      </ListGroup.Item>
-                      <ListGroup.Item>Começo: {employee.start}</ListGroup.Item>
-                      <ListGroup.Item>
-                        Término: {employee.finish}
-                      </ListGroup.Item>
-                    </ListGroup>
-                    <Card.Body>
-                      <Button
-                        variant="danger"
-                        onClick={() => deleteEmployee(employee.id)}
-                      >
-                        Remover
-                      </Button>
-                    </Card.Body>
-                  </Card>
+              <Card style={{ width: "18rem" }} key={employee.name}>
+                <Card.Img
+                  variant="top"
+                  src={employee.image}
+                  style={{ backgroundColor: "#025b8a" }}
+                />
+                <Card.Body style={{ backgroundColor: "#262626" }}>
+                  <Card.Title
+                    style={{ backgroundColor: "inherit", color: "#fff" }}
+                  >
+                    {employee.name}
+                  </Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item
+                    style={{ backgroundColor: "#262626", color: "#fff" }}
+                  >
+                    Nascimento: {employee.bornDate}
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    style={{ backgroundColor: "#262626", color: "#fff" }}
+                  >
+                    Cargo: {employee.position}
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    style={{ backgroundColor: "#262626", color: "#fff" }}
+                  >
+                    Contrato: {employee.contractType}
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    style={{ backgroundColor: "#262626", color: "#fff" }}
+                  >
+                    Salário: R$
+                    {parseFloat(employee.wage).toLocaleString("pt-BR")}
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    style={{ backgroundColor: "#262626", color: "#fff" }}
+                  >
+                    Começo: {employee.start}
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    style={{ backgroundColor: "#262626", color: "#fff" }}
+                  >
+                    Término: {employee.finish}
+                  </ListGroup.Item>
+                </ListGroup>
+                <Card.Body style={{ backgroundColor: "#262626" }}>
+                  <Button
+                    variant="danger"
+                    onClick={() => deleteEmployee(employee.id)}
+                  >
+                    Remover
+                  </Button>
+                </Card.Body>
+              </Card>
             ))}
           </article>
         </>
       ) : (
-          
-        <h3>A lista está vazia.</h3>
+        <h2
+          style={{ textAlign: "center", fontSize: "1.5rem", marginTop: "5dvh" }}
+        >
+          A lista está vazia.
+        </h2>
       )}
     </>
   );
